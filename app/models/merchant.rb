988644a153c1b65f2c793f1987c0711deeed9e23
Leticia <leticiafernandes777@gmail.com>
2023-11-14 22:59:44 -0300

    [ISSUE-11] Add validation for uid, reference and disbursement_frequency

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class Merchant < ApplicationRecord
+  VALID_DISBURSEMENT_FREQUENCIES = [
+    WEEKLY  = "WEEKLY",
+    DAILY  = "DAILY",
+  ].freeze
+
+  validates :uid,
+    presence: true,
+    uniqueness: true
+
+  validates :reference,
+    presence: true,
+    uniqueness: true
+
+  validates :disbursement_frequency,
+    inclusion: {
+      in: VALID_DISBURSEMENT_FREQUENCIES,
+      message: "Invalid Disbursement Frequency"
+    }
 end
