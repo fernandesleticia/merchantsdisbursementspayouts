7a2ee87419ad5495db0aff805c843d56587f86df
Leticia <leticiafernandes777@gmail.com>
2023-11-14 22:38:44 -0300

    [ISSUE-9] Introduce BaseCsvImporter class for CSV processing - Create a new class BaseCsvImporter to handle CSV import operations. - Define class constants BATCH_SIZE and COMMON_DELIMITERS for configuration. - Implement methods for initializing, sniffing column separators, and running the import process. - Enhance batch processing and error handling for a more robust CSV import workflow.

---
+++ app/importers/base_csv_importer.rb
@@ -0,0 +1,55 @@
+class BaseCsvImporter
+  BATCH_SIZE = 20
+  COMMON_DELIMITERS = ['","', '";"', "\"\t\""].freeze
+
+  attr_accessor :input_csv_file, :result_output_stream
+  
+  def initialize(input_csv_file: nil)
+    @input_csv_file = input_csv_file
+  end
+
+  def sniff_column_separator(path)
+    first_line = File.open(path).first
+    return unless first_line
+  
+    snif = {}
+    COMMON_DELIMITERS.each do |delim| 
+      snif[delim] = first_line.count(delim)
+    end
+    snif = snif.sort { |a,b| b[1]<=>a[1] }
+  
+    snif[0][0][1] if snif.size > 0
+  end
+
+  def run
+    run_time = Time.current.strftime("%Y-%m-%d_%H%M%S")
+    @result_output_stream = File.open("/tmp/result-#{run_time}.csv", "w")
+    @batch= []
+    result_output_stream.write(result_output_stream_header)
+
+    start = Time.current
+
+    col_sep = sniff_column_separator(input_csv_file)
+
+    CSV.foreach(input_csv_file, headers: true, encoding: "ISO-8859-1", col_sep: col_sep).with_index do |row|
+      next if row.to_hash.keys.count.zero?
+      @batch << row
+      process_batch if @batch.count == BATCH_SIZE
+    end
+
+    process_batch if @batch.any?
+  ensure
+    @result_output_stream.close
+  end
+
+  def write_dot(batch_with_error)
+    $stdout.write(batch_with_error ? "E" : ".")
+    $stdout.flush
+  end
+
+  def validates_data(row)
+    return if valid_row?(row)
+
+    raise StandardError, "Data missing"
+  end
+end
